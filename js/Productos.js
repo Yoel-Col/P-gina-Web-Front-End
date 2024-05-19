a2727d9496ad11f6a3015b0d04411c45e9e4f4ba
SEBASTIANULIBARRI <sebastiannahuelulibarri@gmail.com>
2024-05-19 19:53:58 -0300

    Correccion detalles productos

--- js/Productos.js
+++ js/Productos.js
@@ -13,37 +13,21 @@ function getUrlSpecific(done, id) {
 }
 
 getUrlSpecific((data) => {
-  data.forEach((element) => {
-    let figure = document.createElement("figure");
-    let img = document.createElement("img");
-    let div = document.createElement("div");
-    let a = document.createElement("a");
-    let p = document.createElement("p");
-    let figcaption = document.createElement("figcaption");
-    let precio = document.createElement("figcaption");
-    //pogo el titulo de la img
-    figcaption.innerHTML = element.title;
-    //pogo el precio de la img
-    precio.innerHTML = "$" + element.precio;
-    //agrego la clase para poder formatear desde CSS
-    img.classList.add("producto-img");
-    //apunto a la imagen que esta localizada en la carpeta del proyecto pero guardada en la api
-    img.src = element.url_img;
-    //agrego la clase para poder formatear desde CSS
-    figure.classList.add("producto");
-    figure.classList.add("p" + element.id);
-    //agrego el ID paara poder mostrar una descripcion unica del producto
-    figure.id = element.id;
-    //agrego los hijos dentro de figura para crear la tarjeta
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    figure.appendChild(precio);
-    //meto todo dentro de una etiqueta a para poder darle el salto a otra pagina (en este momento lo muestra dentro de la misma pagina pero la idea original era cambiar de pagina.)
-    a.appendChild(figure);
-    //lo agrego al DOM para verlo desde la pagina html
-    document.getElementById("Container").appendChild(a);
-    //creo un eventListener para todas las tarjetas que se agregaron en la linea anterior y poder mostrar una descripcion cuando sea necesario
-    var productos = document.querySelectorAll(".container>a");
+  data.forEach((element) => { 
+    console.log(element)
+    const tarjetaProducto = document.createRange().createContextualFragment(
+
+        `<a class ="nn">
+      <figure class="producto p${element.id}" id="${element.id}">
+        <img class="producto-img" src="${element.url_img}">
+        <figcaption>${element.title}</figcaption>
+        <figcaption>$${element.precio}</figcaption>
+      </figure>
+    </a>`)
+    console.log("tarj" , tarjetaProducto);
+    document.getElementById("Container").appendChild(tarjetaProducto);
+
+    let productos = document.querySelectorAll(".container>a");
     productos.forEach((element) => {
       element.addEventListener("click", mostrarDescripcion);
     });
@@ -69,8 +53,7 @@ function mostrarDescripcion(event) {
 
     document.querySelector(".muestra img").src = data.url_img;
     document.querySelector(".muestra .titulo").innerHTML = data.title;
-    document.querySelector(".muestra .descripcion").innerHTML =
-      data.descripcion;
+    document.querySelector(".muestra .descripcion").innerHTML = data.descripcion;
     document.querySelector(".muestra .precio").innerHTML =
       "Precio: $" + data.precio;
     document.querySelector(".container").classList.add("oculto");
